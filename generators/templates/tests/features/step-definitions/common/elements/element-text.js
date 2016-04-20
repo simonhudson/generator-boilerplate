9a3e239cfd48012f1bba17009852524fa2d7b1cd
Simon Hudson <simon.hudson@experian.com>
2016-04-20 07:33:47 +0100

    Update step-def phrasing

--- generators/templates/tests/features/step-definitions/common/elements/element-text.js
+++ generators/templates/tests/features/step-definitions/common/elements/element-text.js
@@ -2,7 +2,7 @@ var assert = require('assert');
 
 module.exports = function () {
 
-    this.Then(/^the "(.*)" element should contain the text "(.*)"$/, function(selector, expected) {
+    this.Then(/^the "(.*)" element contains the text "(.*)"$/, function(selector, expected) {
         var actual = browser.getText(selector);
         assert.equal(expected, actual, 'Unexpected "' + selector + '" value (expected: "' + expected + '", actual: "' + actual + '")');
     });
