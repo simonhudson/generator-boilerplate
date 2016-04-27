ab8da460dc61cab02e4cc4eca52f18009c0a0fa6
Simon Hudson <simon.hudson@experian.com>
2016-04-27 14:30:19 +0100

    Add wait to visible/not visible step-def

--- generators/templates/tests/features/step-definitions/common/elements/element-not-visible.js
+++ generators/templates/tests/features/step-definitions/common/elements/element-not-visible.js
@@ -3,6 +3,7 @@ var assert = require('assert');
 module.exports = function () {
 
     this.Then(/^the "(.*)" element is not visible$/, function(selector) {
+        browser.waitForVisible(selector, 500, true);
         assert.equal(browser.isVisible(selector), false, selector + ' element is visible on the page');
     });
 
--- generators/templates/tests/features/step-definitions/common/elements/element-visible.js
+++ generators/templates/tests/features/step-definitions/common/elements/element-visible.js
@@ -3,6 +3,7 @@ var assert = require('assert');
 module.exports = function () {
 
     this.Then(/^the "(.*)" element is visible$/, function(selector) {
+        browser.waitForVisible(selector);
         assert.equal(browser.isVisible(selector), true, selector + ' element is not visible on the page');
     });
 
